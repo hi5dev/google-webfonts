fb9e0a0ba904168fcf5af08c1ef49fa12be25746
bokmann <dbock@codesherpas.com>
2012-10-01 18:42:15 -0400

    fixing someone elses borked typo in the http method resolution code.

--- lib/google-webfonts/helper.rb
+++ lib/google-webfonts/helper.rb
@@ -1,13 +1,13 @@
 module Google
   module Webfonts
-  
+
     # Public: Helper module that includes the google_webfonts_link_tag method.
     # This module is automatically included in your Rails view helpers.
     module Helper
       include ActionView::Helpers::TagHelper
-      
+
       # Public: Generates a Google Webfonts link tag
-      # 
+      #
       # options - The font options. This can be a String, Symbol, or Hash, or
       #           a combination of all three. If you need to specify a font
       #           size, use a Hash, otherwise a String or Symbol will work.
@@ -16,17 +16,17 @@ module Google
       #
       #   google_webfonts_link_tag "Droid Sans"
       #   # => '<link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css" />'
-      # 
+      #
       #   google_webfonts_link_tag :droid_sans
       #   # => '<link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css" />'
-      # 
+      #
       #   google_webfonts_link_tag :droid_sans => [400, 700]
       #   # => '<link href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet" type="text/css" />'
-      # 
+      #
       #   google_webfonts_link_tag :droid_sans => [400, 700],
       #                            :yanone_kaffeesatz => [300, 400]
       #   # => '<link href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Yanone+Kaffeesatz:300,400" rel="stylesheet" type="text/css" />'
-      # 
+      #
       #   google_webfonts_link_tag "Droid Sans",
       #                            :yanone_kaffeesatz => 400
       #   # => '<link href="http://fonts.googleapis.com/css?family=Droid+Sans|Yanone+Kaffeesatz:400" rel="stylesheet" type="text/css" />'
@@ -37,42 +37,42 @@ module Google
       # Raises ArgumentError if a size is not a String or Fixnum.
       def google_webfonts_link_tag(*options)
         raise ArgumentError, "expected at least one font" if options.empty?
-        
+
         fonts = []
-        
+
         options.each do |option|
           case option.class.to_s
           when "Symbol", "String"
             # titleize the font name
             font_name = option.to_s.titleize
-            
+
             # replace any spaces with pluses
             font_name = font_name.gsub(" ", "+")
-            
+
             # include the font
             fonts << font_name
           when "Hash"
             fonts += option.inject([]) do |result, (font_name, sizes)|
               # ensure sizes is an Array
               sizes = Array(sizes)
-              
+
               sizes.all? do |size|
                 unless size.class == Fixnum || size.class == String
                   raise ArgumentError, "expected a Fixnum or String, got a #{size.class}"
                 end
               end
-              
+
               # convert font name into a String
               font_name = font_name.to_s
-              
+
               # replace underscores with spaces
               # and titleize the font name
               font_name = font_name.gsub("_", " ")
               font_name = font_name.titleize
-              
-              # convert the spaces into pluses 
+
+              # convert the spaces into pluses
               font_name = font_name.gsub(" ", "+")
-              
+
               # return font_name:sizes where
               # sizes is a comma separated list
               result << "#{font_name}:#{sizes.join(",")}"
@@ -81,7 +81,7 @@ module Google
             raise ArgumentError, "expected a String, Symbol, or a Hash, got a #{option.class}"
           end
         end
-        
+
         # the fonts are separated by pipes
         family = fonts.join("|")
 
@@ -92,13 +92,12 @@ module Google
         tag 'link', {
             :rel  => :stylesheet,
             :type => Mime::CSS,
-            :href => "http://fonts.googleapis.com/css?family=#{family}"
             :href => "#{request_method}://fonts.googleapis.com/css?family=#{family}"
           },
           false,
           false
       end
     end
-    
+
   end
 end
